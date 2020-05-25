9ec14eb63844ecb751a2d5e4b9b358723ffc33ad
nan8574 <37944823+jnj9938@users.noreply.github.com>
2020-05-26 00:21:35 +0900

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -40,3 +40,14 @@
         target: "#sideNav",
     });
 })(jQuery); // End of use strict
+
+$(document).ready(function(e){
+    $('.search-panel .dropdown-menu').find('a').click(function(e) {
+        e.preventDefault();
+        var param = $(this).attr("href").replace("#","");
+        var concept = $(this).text();
+        $('.search-panel span#search_concept').text(concept);
+        $('.input-group #search_param').val(param);
+    });
+});
+
